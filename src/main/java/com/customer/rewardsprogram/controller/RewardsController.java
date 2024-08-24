90d4e5cec391ebfde71dd3d98d9a89c3431a454e
shailamogalapu57 <shailasudarshan.m92@gmail.com>
2024-08-24 13:41:00 -0500

    Added GET API with data set

--- src/main/java/com/customer/rewardsprogram/controller/RewardsController.java
+++ src/main/java/com/customer/rewardsprogram/controller/RewardsController.java
@@ -1,8 +1,12 @@
 package com.customer.rewardsprogram.controller;
 
+import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,8 +26,30 @@ public class RewardsController {
 	}
 
 	@PostMapping("/calculate")
-	public Map<String,Object> calculateRewards(@RequestBody Map<String, List<RewardsTransaction>> customerTransactions) {
-		 
+	public Map<String, Object> calculateRewards(
+			@RequestBody Map<String, List<RewardsTransaction>> customerTransactions) {
 		return rewardsService.getRewardPoints(customerTransactions);
 	}
+
+	@GetMapping("/calculate")
+	public Map<String, Object> calculateRewardsForDataset() {
+		 Map<String, List<RewardsTransaction>> customerTransactions = new HashMap<>();
+
+	        customerTransactions.put("C1", Arrays.asList(
+	                new RewardsTransaction("C1", 120.0, LocalDate.of(2024, 6, 15)),
+	                new RewardsTransaction("C1", 75.0, LocalDate.of(2024, 7, 10)),
+	                new RewardsTransaction("C1", 50.0, LocalDate.of(2024, 8, 5)),
+	                new RewardsTransaction("C1", 110.0, LocalDate.of(2024, 6, 20))
+	        ));
+
+	        customerTransactions.put("C2", Arrays.asList(
+	                new RewardsTransaction("C2", 100.0, LocalDate.of(2024, 6, 10)),
+	                new RewardsTransaction("C2", 85.0, LocalDate.of(2024, 7, 12)),
+	                new RewardsTransaction("C2", 130.0, LocalDate.of(2024, 8, 15))
+	        ));
+	        
+	     // Reuse the existing POST logic to calculate rewards
+	        return calculateRewards(customerTransactions);
+	}
+	
 }
